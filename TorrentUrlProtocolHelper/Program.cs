055ba66b428dd8d3a59fba0e6f4b81cce4e9fe1b
Alex Németh <alex19930329@gmail.com>
2025-02-15 02:58:46 +0100

    Refactoring

--- TorrentUrlProtocolHelper/Program.cs
+++ TorrentUrlProtocolHelper/Program.cs
@@ -1,107 +1,102 @@
-﻿internal class Program
+﻿using TorrentUrlProtocolHelper;
+
+internal class Program
 {
+    private enum OperationMode
+    {
+        Unknown,
+        AddTorrent,
+        RegisterMagnetProtocol,
+    }
+
     private static void Main(string[] args)
     {
+
         try
         {
+            var operationMode = GetOperationModeFromArgs(args);
 
+            switch (operationMode)
+            {
+                case OperationMode.AddTorrent:
+                    string torrentName = TorrentHandler.AddTorrent(args[1]);
+                    ShowToastNotification("Torrent added", torrentName);
+                    break;
 
-            RegisterMagnetProtocol();
+                case OperationMode.RegisterMagnetProtocol:
+                    RegisterMagnetProtocol();
+                    ShowToastNotification("Registration", "Registration complete.");
+                    break;
 
-            string magnetLink = GetMagnetLinkFromArgs(args);
-
-            AddTorrent(magnetLink);
-
-            string torrentName = ExtractNameFromMagnetLink(magnetLink);
-            ShowToastNotification("Torrent added", torrentName);
+                default:
+                    ShowUsageInfo();
+                    break;
+            }
         }
         catch (Exception ex)
         {
             ShowToastNotification("Error", ex.Message);
         }
+    }
 
-
-
-        static void RegisterMagnetProtocol()
+    private static OperationMode GetOperationModeFromArgs(string[] args)
+    {
+        if (args.Length == 1 && args[0] == "-register")
         {
-            const string keyPath = $@"Software\Classes\magnet";
-
-            // Delete the key if it already exists
-            try
-            {
-                Microsoft.Win32.Registry.CurrentUser.DeleteSubKeyTree(keyPath);
-            }
-            catch { /* Ignore if not exists */ }
-
-            // Create the key
-            using var newKey = Microsoft.Win32.Registry.CurrentUser.CreateSubKey(keyPath);
-
-            newKey.SetValue("", "URL:Magnet Protocol");
-            newKey.SetValue("URL Protocol", "");
-
-            using var registryKeyShell = newKey.CreateSubKey("shell");
-            using var registryKeyOpen = registryKeyShell.CreateSubKey("open");
-            using var registryKeyCommand = registryKeyOpen.CreateSubKey("command");
-
-            registryKeyCommand.SetValue("", $@"""{Environment.ProcessPath}"" ""%1""");
+            return OperationMode.RegisterMagnetProtocol;
         }
 
-        static string GetMagnetLinkFromArgs(string[] args)
+        if (args.Length == 2 && args[0] == "-addtorrent")
         {
-            if (args.Length == 0)
-            {
-                throw new ArgumentException("No magnet link provided as argument.");
-            }
-
-            string magnetLink = args[0];
-
-            if (magnetLink.StartsWith("magnet:") == false)
-            {
-                throw new ArgumentException("Invalid magnet link provided as argument.");
-            }
-
-            return magnetLink;
+            return OperationMode.AddTorrent;
         }
 
-        static void AddTorrent(string magnetLink)
+        return OperationMode.Unknown;
+    }
+
+    static void RegisterMagnetProtocol()
+    {
+        const string keyPath = $@"Software\Classes\magnet";
+
+        // Delete the key if it already exists
+        try
         {
-            var qBittorrentConfig = TorrentUrlProtocolHelper.Configuration.QBittorrentConfig;
-
-            HttpClientHandler httpClientHandler = new() { CookieContainer = new System.Net.CookieContainer() }; ;
-            HttpClient httpClient = new(httpClientHandler);
-
-            var loginData = new FormUrlEncodedContent(
-            [
-                new KeyValuePair<string, string>("username", qBittorrentConfig.Username),
-        new KeyValuePair<string, string>("password", qBittorrentConfig.Password)
-            ]);
-
-            httpClient.PostAsync($"{qBittorrentConfig.BaseUrl}auth/login", loginData).Wait();
-
-            var addData = new FormUrlEncodedContent([new KeyValuePair<string, string>("urls", magnetLink)]);
-            httpClient.PostAsync($"{qBittorrentConfig.BaseUrl}torrents/add", addData).Wait();
+            Microsoft.Win32.Registry.CurrentUser.DeleteSubKeyTree(keyPath);
         }
+        catch { /* Ignore if not exists */ }
 
-        static void ShowToastNotification(string title, string message)
-        {
-            string logoFilePath = Path.Combine(AppContext.BaseDirectory, "Resources", "qbittorrent_logo.png");
+        // Create the key
+        using var newKey = Microsoft.Win32.Registry.CurrentUser.CreateSubKey(keyPath);
 
-            new Microsoft.Toolkit.Uwp.Notifications.ToastContentBuilder()
-                .AddAppLogoOverride(new Uri(logoFilePath))
-                .AddText(title)
-                .AddText(message)
-                .Show();
-        }
+        newKey.SetValue("", "URL:Magnet Protocol");
+        newKey.SetValue("URL Protocol", "");
 
-        static string ExtractNameFromMagnetLink(string magnetLink)
-        {
-            var uri = new Uri(magnetLink);
-            string query = uri.Query;
+        using var registryKeyShell = newKey.CreateSubKey("shell");
+        using var registryKeyOpen = registryKeyShell.CreateSubKey("open");
+        using var registryKeyCommand = registryKeyOpen.CreateSubKey("command");
 
-            var parameters = System.Web.HttpUtility.ParseQueryString(query);
-            string? name = parameters["dn"];
+        registryKeyCommand.SetValue("", $@"""{Environment.ProcessPath}"" -addtorrent ""%1""");
+    }
 
-            return name is null ? "Could not extract name" : System.Web.HttpUtility.UrlDecode(name);
-        }
+    private static void ShowUsageInfo()
+    {
+        const string usageInfo = """
+            Usage:
+            -register: Register the magnet protocol to use this application.
+            -addtorrent <magnet_link>: Add a torrent using the magnet link.
+            """;
+
+        MessageBox.Show(usageInfo);
+    }
+
+    static void ShowToastNotification(string title, string message)
+    {
+        string logoFilePath = Path.Combine(AppContext.BaseDirectory, "Resources", "qbittorrent_logo.png");
+
+        new Microsoft.Toolkit.Uwp.Notifications.ToastContentBuilder()
+            .AddAppLogoOverride(new Uri(logoFilePath))
+            .AddText(title)
+            .AddText(message)
+            .Show();
     }
 }
---
+++ TorrentUrlProtocolHelper/TorrentHandler.cs
@@ -0,0 +1,52 @@
+﻿namespace TorrentUrlProtocolHelper;
+
+internal class TorrentHandler
+{
+    public static string AddTorrent(string magnetLink)
+    {
+        AddTorrentOnWebApi(magnetLink);
+
+        return ExtractNameFromMagnetLink(magnetLink);
+    }
+
+    private static void AddTorrentOnWebApi(string magnetLink)
+    {
+        var qBittorrentConfig = Configuration.QBittorrentConfig;
+
+        var httpClientHandler = new HttpClientHandler() { CookieContainer = new System.Net.CookieContainer() }; ;
+        var httpClient = new HttpClient(httpClientHandler);
+
+        var loginData = new FormUrlEncodedContent(
+        [
+            new KeyValuePair<string, string>("username", qBittorrentConfig.Username),
+            new KeyValuePair<string, string>("password", qBittorrentConfig.Password)
+        ]);
+
+        var loginResponse = httpClient.PostAsync($"{qBittorrentConfig.BaseUrl}auth/login", loginData).Result;
+
+        if (loginResponse.IsSuccessStatusCode == false)
+        {
+            throw new Exception($"Login failed: {loginResponse.StatusCode}");
+        }
+
+        var addData = new FormUrlEncodedContent([new KeyValuePair<string, string>("urls", magnetLink)]);
+
+        var addResponse = httpClient.PostAsync($"{qBittorrentConfig.BaseUrl}torrents/add", addData).Result;
+
+        if (addResponse.IsSuccessStatusCode == false)
+        {
+            throw new Exception($"Adding torrent failed: {addResponse.StatusCode}");
+        }
+    }
+
+    static string ExtractNameFromMagnetLink(string magnetLink)
+    {
+        var uri = new Uri(magnetLink);
+        string query = uri.Query;
+
+        var parameters = System.Web.HttpUtility.ParseQueryString(query);
+        string? name = parameters["dn"];
+
+        return name is null ? "*Could not extract torrent name*" : System.Web.HttpUtility.UrlDecode(name);
+    }
+}
